ce5448b054adb0873bff5f8dea6030b3e8a1964d
mingshun <gutspot@qq.com>
2013-09-30 08:48:35 +0000

    Corrected a mistake within uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
   exit ();
 }
 
-$option_names = ('varnish_helper_auto_clean_tasks');
+$option_names = array('varnish_helper_auto_clean_tasks');
 
 if (!is_multisite()) {
 	foreach ($option_names as $option_name) {
